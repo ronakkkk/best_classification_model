4d5b04e03ae4c9e4b4f9681029d7efe3eceace6a
Ronak Bhagchandani <37010825+ronakkkk@users.noreply.github.com>
2021-08-17 00:34:38 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.1.0'
+VERSION = '0.1.2'
 PACKAGE_NAME = 'BestClassificationModel'
 AUTHOR = 'Ronak Bhagchandani'
 AUTHOR_EMAIL = 'rishibhagchandani123@gmail.com'
